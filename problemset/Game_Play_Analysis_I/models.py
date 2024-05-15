5e7a2ab1b846d0d4825541a7d3469d890605cfd7
Vadim <103333041+dmBra1n@users.noreply.github.com>
2024-05-15 15:10:04 +0300

    I have completed the task "511"

--- problemset/Game_Play_Analysis_I/models.py
+++ problemset/Game_Play_Analysis_I/models.py
@@ -1,6 +1,24 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, MetaData, Table
+import datetime
+from typing import Annotated
+
+from sqlalchemy import Column, DateTime, Integer, MetaData, Table
+from sqlalchemy.orm import Mapped, mapped_column
 
 from utils.database import Base
+
+intpk = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
+
+
+class ActivityOrm(Base):
+    __tablename__ = "activity"
+
+    id: Mapped[intpk]
+    player_id: Mapped[int]
+    device_id: Mapped[int]
+    event_date: Mapped[datetime.date]
+    games_played: Mapped[int]
+
+
 
 metadata_obj = MetaData()
 
