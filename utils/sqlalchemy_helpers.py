7647f10768c8ea3bf1efa1e5224a7098a5af76fc
Vadim <103333041+dmBra1n@users.noreply.github.com>
2024-05-19 23:18:38 +0300

    Fixing sqlalchemy_helpers

--- utils/sqlalchemy_helpers.py
+++ utils/sqlalchemy_helpers.py
@@ -1,7 +1,4 @@
-from typing import Union
-
-from sqlalchemy.engine.cursor import CursorResult
-from sqlalchemy.engine.result import ChunkedIteratorResult
+from sqlalchemy.engine.cursor import ResultProxy
 from tabulate import tabulate
 
 
@@ -12,30 +9,22 @@ class DisplayUtils:
     """
 
     @staticmethod
-    def display_orm(result: Union[ChunkedIteratorResult, None]) -> None:
+    def display_results(result: ResultProxy) -> None:
         """
-        Displays the attribute values of SQLAlchemy ORM model objects in a tabular format.
-        :param:  A list of SQLAlchemy ORM model objects to display.
-        :return: None
-        """
-        if result:
-            objects = result.scalars().all()
-            headers = objects[0].__table__.columns.keys()
-            data = [tuple(getattr(obj, attr) for attr in headers) for obj in objects]
-            print(tabulate(data, headers=headers, tablefmt="psql"))
-        else:
-            print("No ORM objects to display")
+        Displays the results of a SQLAlchemy query in a tabular format.
 
-    @staticmethod
-    def display_core(result: Union[CursorResult, None]) -> None:
-        """
-        Displays the attribute values of SQLAlchemy core query results in a tabular format.
-        :param: SQLAlchemy core query result to display.
+        This method expects a raw SQLAlchemy query result
+        (without additional processing or formatting)
+        and displays it in a tabular format using the tabulate library.
+
+        :param: The result of a SQLAlchemy query (ResultProxy object).
         :return: None
         """
-        if result:
-            headers = result.keys()
-            data = result.all()
-            print(tabulate(data, headers=headers, tablefmt="psql"))
+        query_result = result.all()
+        if query_result:
+            column_names = result.keys()
+            print(tabulate(
+                query_result, headers=column_names, tablefmt="psql"
+            ))
         else:
-            print("No core query results to display")
+            print("No objects to display")
