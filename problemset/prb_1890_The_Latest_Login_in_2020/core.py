2ad704073d5210dcc965f95ed7733aadab741476
Vadim <103333041+dmBra1n@users.noreply.github.com>
2024-05-24 23:21:03 +0300

    fixed flake8 violations in "1890. The Latest Login in 2020"

--- problemset/prb_1890_The_Latest_Login_in_2020/core.py
+++ problemset/prb_1890_The_Latest_Login_in_2020/core.py
@@ -34,7 +34,7 @@ class SyncCore:
             query = (
                 """SELECT user_id, MAX(time_stamp) as last_stamp
                 FROM Logins
-                WHERE EXTRACT(YEAR FROM time_stamp) = 2020 
+                WHERE EXTRACT(YEAR FROM time_stamp) = 2020
                 GROUP BY user_id;"""
             )
             result = session.execute(text(query))
