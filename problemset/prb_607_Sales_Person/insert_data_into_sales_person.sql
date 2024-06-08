2ae852139cac8d6df6b7e5ea43080905a8dc6366
Vadim <103333041+dmBra1n@users.noreply.github.com>
2024-06-08 23:31:22 +0300

    Added a new task "607. Sales Person": files with added data

---
+++ problemset/prb_607_Sales_Person/insert_data_into_sales_person.sql
@@ -0,0 +1,5 @@
+insert into SalesPerson (sales_id, name, salary, commission_rate, hire_date) values ('1', 'John', '100000', '6', '4/1/2006')
+insert into SalesPerson (sales_id, name, salary, commission_rate, hire_date) values ('2', 'Amy', '12000', '5', '5/1/2010')
+insert into SalesPerson (sales_id, name, salary, commission_rate, hire_date) values ('3', 'Mark', '65000', '12', '12/25/2008')
+insert into SalesPerson (sales_id, name, salary, commission_rate, hire_date) values ('4', 'Pam', '25000', '25', '1/1/2005')
+insert into SalesPerson (sales_id, name, salary, commission_rate, hire_date) values ('5', 'Alex', '5000', '10', '2/3/2007')
