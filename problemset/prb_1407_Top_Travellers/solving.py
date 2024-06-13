bfed0bac43ecb7bec476f6b69044efba8802e7e6
Vadim <103333041+dmBra1n@users.noreply.github.com>
2024-06-13 11:11:10 +0300

    Fixed a core query in the task "1407. Top Travellers"

--- problemset/prb_1407_Top_Travellers/solving.py
+++ problemset/prb_1407_Top_Travellers/solving.py
@@ -12,7 +12,7 @@ class TopTravellersCore(BaseCore):
                 """SELECT u.name,
                     COALESCE(SUM(r.distance), 0) AS travelled_distance
                     FROM Users u LEFT JOIN Rides r ON u.id = r.user_id
-                    GROUP BY u.id
+                    GROUP BY u.id, u.name
                     ORDER BY travelled_distance DESC, u.name;""")
             result = session.execute(text(query))
             DisplayUtils.display_results(result)
